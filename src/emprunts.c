b024f990fefaa284d79f33bf08263fca9862d930
AlphaGC8 <guillaume.canaud08@gmail.com>
2026-04-23 11:30:20 +0200

    [200~Dans ce commit, j'ai créé tout les fichiers majeurs (en c, en h et les .txt)

---
+++ src/emprunts.c
@@ -0,0 +1,4 @@
+// verifier s'il reste des exemplaires en stock et si l'utilisateur peut emprunter (si etudiant ou prof)
+// Calcul des dates
+
+#include <time.h>
--- src/main.c
+++ src/main.c
@@ -1,1 +1,2 @@
+// S'occupera des Menus et boucle prncipales
 #include <stdio.h>
