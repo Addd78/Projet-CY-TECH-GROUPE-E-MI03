18766bdaf28e12604fbbf1860cac327003f502c2
e-skadraou <sayfeddine.kadraoui2@gmail.com>
2026-04-29 15:00:59 +0200

    Refactor borrowing verification and user roles
    Refactor user borrowing logic and update structure.

--- src/emprunts.c
+++ src/emprunts.c
@@ -1,70 +1,64 @@
 // verifier s'il reste des exemplaires en stock et si l'utilisateur peut emprunter (si etudiant ou prof)
-#include <stdio.h>
-
-typedef enum{
-professeur,
-etudiant
-}Role;
-
-typedef struct{
-Role role;
-
-}utilisateur;
-
-
-
+include <stdio.h>
+ 
+typedef enum {
+    professeur,
+    etudiant
+} Role;
+ 
+typedef struct {
+    Role role;
+    int  peut_emprunter;
+} utilisateur;
+ 
+typedef struct {
+    int exemplaires_restants;
+} livre;
+ 
 int livre_max;
-int temps_max;
-
-
-
-
-
-
-int choix(utilisateur*personne){
-if(personne->role==professeur){
-livre_max=5;
-temps_max=180;
+int temps_max;            // voir si possible de faire tourner avec les 2 max ds la struct livre 
+ 
+void choix(utilisateur *personne) {
+    if (personne->role == professeur) {
+        livre_max = 5;
+        temps_max = 180;
+    } else {
+        livre_max = 3;
+        temps_max = 120;
+    }
 }
-else{
-    livre_max=3;
-    temps_max=120;
-    } 
-
-}  
-
-int verif_emprunt(utilisateur*personne,int temps_actuel,int livres_emprunts){
-    choix (personne,int nb_livres_retard);
-    
-    if(livres_emprunts>livre_max){
-        printf("limite de livres depassé: %d/%d",livres_emprunts,livre_max);
+ 
+int verif_emprunt(utilisateur *personne, int temps_actuel,
+                  int livres_empruntes, int nb_livres_retard,
+                  livre *l) {
+ 
+    choix(personne);
+ 
+    if (personne->peut_emprunter == 0) {
+        printf("Refus : votre compte ne permet pas d'emprunter.\n");
         return 0;
     }
-      
-    if(nb_livres_retard>0){
-        printf("vous avez %d livres de retard,rendez les !",nb_livres_retard);
-        return 1;
-      
-        
-        
-    if(temps_actuel>temps_max){
-        printf("temps d'emprunt depassé:aucun emprunt possible  %d/%d",temps_actuel,temps_max);
-        return 0}
-        
-        printf("emprunt autorisé");
-        return 1;
-        
+ 
+    if (l->exemplaires_restants <= 0) {    //a le droit d'emprunter mais livre en rupture
+        printf("Refus : plus aucun exemplaire disponible.\n");
+        return 0;
+    }
+ 
+    if (nb_livres_retard > 0) {
+        printf("Refus : vous avez %d livre(s) en retard, rendez-les d'abord !\n", nb_livres_retard);
+        return 0;
+    }
+ 
+    if (livres_empruntes >= livre_max) {
+        printf("Refus : limite de livres depassee (%d/%d).\n", livres_empruntes, livre_max);
+        return 0;
+    }
+ 
+    if (temps_actuel > temps_max) {
+        printf("Refus : temps depasse (%ds/%ds).\n", temps_actuel, temps_max);
+        return 0;
+    }
+ 
+    printf("Emprunt autorise ! (%d/%d livres)\n", livres_empruntes + 1, livre_max);
+    return 1;
 }
-        
-        
-        
-        
-        
-        
-        
-        )
-    
-}
-// Calcul des dates
-
-#include <time.h>
